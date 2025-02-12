6ba3eb4363ea3505000a93849561031273cfa572
Nitinjr812 <nitinjr812@gmail.com>
2025-02-12 09:11:02 +0530

    Resolved conflicts and updated FreshCart

--- src/Services/ProtectedRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-const ProtectedRoutes = () => {
-    const auth = localStorage.getItem("logged");
-    return auth ? <Outlet /> : <Navigate to={"/"} />
-
-};
-export default ProtectedRoutes
